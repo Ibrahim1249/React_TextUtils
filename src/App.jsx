be97b17e5fb624352205ca12e575c0b50bc83a9c
Ibrahim1249 <mominibrahim98.im85@gmail.com>
2024-06-21 19:06:39 +0530

    done assignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,17 @@ import NavBar from './NavBar'
 import "./App.css"
 import TextUtils from './TextUtils'
 import Footer from './Footer'
+import Context from './Context'
+
 
 function App() {
   return (
     <>
+        <Context>
      <NavBar />
      <TextUtils />
      <Footer/>
+     </Context>
     </>
   )
 }
--- src/Preview.jsx
+++ src/Preview.jsx
@@ -1,14 +1,17 @@
 
 import React from 'react'
-
+import { useContext } from 'react';
+import { textContext } from './Context';
 function Preview() {
+  const {state } = useContext(textContext);
+
   return (
     <>
      <div className="preview-container">
        <h4>Preview Document</h4>
 
        <div className="preview-area">
-         <textarea name="" id="" disabled value="hello"></textarea>
+         <textarea name="" id="" disabled value={state.text}></textarea>
        </div>
      </div>
     </>
